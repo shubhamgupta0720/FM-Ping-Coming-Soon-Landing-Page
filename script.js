22e40c99d3c851dcc0541141b26ad7297db978d0
Shubham <shubhamgupta0720@gmail.com>
2024-02-21 22:31:37 +0530

    Added mobile design

---
+++ script.js
@@ -0,0 +1,19 @@
+function formSubmit(event){
+    event.preventDefault();
+    var inputField = document.getElementById('email');
+    var emailError = document.getElementById("email-error");
+    var innerWidth = window.innerWidth;
+    if (inputField.value.trim() === "") {
+        inputField.style.borderColor = "hsl(354, 100%, 66%)"; // Change border color to red
+        emailError.style.visibility = "visible"; // Display the error message
+        if(innerWidth <= 410){
+            console.log("mbfe")
+            emailError.classList.add('active');
+        }
+    } else {
+        inputField.style.borderColor = ""; // Reset border color
+        emailError.style.visibility = "hidden"; // Hide the error message
+        // If email is not empty, you can submit the form here
+        // e.g., event.target.submit();
+    }
+}
